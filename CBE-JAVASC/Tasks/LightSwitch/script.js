b64a4462fe88b3a6c236ada59aa73a8bf5e714db
Stefan Schulz <130909583+Schulz-CM001@users.noreply.github.com>
2023-06-02 14:08:52 +0200

    AddedResults

--- CBE-JAVASC/Tasks/LightSwitch/script.js
+++ CBE-JAVASC/Tasks/LightSwitch/script.js
@@ -1,1 +1,14 @@
-const btn = document.querySelector("button");
+//erste Konstante = body
+const body = document.body;
+//zweite Konstante = button
+const btn = document.querySelector("#button");
+//dritte Konstante = Titel
+const pageTitle = document.querySelector("#pageTitle");
+
+btn.addEventListener("click", function () {
+  body.classList.toggle("dark-mode");
+  btn.classList.toggle("dark-btn");
+  if (body.classList.contains("dark-mode")) {
+    pageTitle.textContent = "Good Night";
+  } else pageTitle.textContent = "Good Morning";
+});
